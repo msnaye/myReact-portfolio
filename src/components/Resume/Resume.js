0631c202d1c4a0cf9f9fd931f1c61c613ef4a15b
msnaye <99111936+msnaye@users.noreply.github.com>
2022-08-12 03:40:53 -0500

    edited resume container

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -2,30 +2,32 @@ import '../../styles/Resume.css';
 import React from "react";
 
 function Resume() {
-    return(
+    return (
         <section className="ResumeContainer">
-            <h2>Resume</h2>
+            <div className="ContText">
+                <h2>Resume</h2>
 
-            <h4>Education</h4>
-            <ul>
-                <li>University of Minnesota</li>
-                <li>Normandale Community College</li>
-                <li>University of Minnesota Bootcamps</li> 
-            </ul>
+                <h4>Education</h4>
+                <ul>
+                    <li>University of Minnesota</li>
+                    <li>Normandale Community College</li>
+                    <li>University of Minnesota Bootcamps</li>
+                </ul>
 
-            <h4>Technologies</h4>
-            <ul>
-                <li>HTML</li>
-                <li>CSS</li>
-                <li>JavaScript</li>
-                <li>Node.js</li>
-                <li>Express.js</li>
-            </ul>
-            <div>
-                My Resume 
-                <a href={require("../../assets/PhotoList/MayraResumeSE.pdf")} download className="download">Download</a>
+                <h4>Technologies</h4>
+                <ul>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JavaScript</li>
+                    <li>Node.js</li>
+                    <li>Express.js</li>
+                </ul>
+                <div>
+                    My Resume
+                    <a href={require("../../assets/PhotoList/MayraResumeSE.pdf")} download className="download">Download</a>
+                </div>
             </div>
-        </section>  
+        </section>
     )
 }
 
